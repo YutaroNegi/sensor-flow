095164bfaffa990d10c1bb80e6a5fb8a7e43c494
Yutaro Negi <souza_yutaro@hotmail.com>
2024-11-30 15:15:28 -0300

    feat: add guard to sensor data creation

--- src/modules/sensor-data/sensor-data.controller.ts
+++ src/modules/sensor-data/sensor-data.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { SensorDataService } from './sensor-data.service';
 import { CreateSensorDataDto } from './dto/create-sensor-data.dto';
 
 @Controller('sensor-data')
 export class SensorDataController {
   constructor(private readonly sensorDataService: SensorDataService) {}
-
+  @UseGuards(AuthGuard('jwt'))
   @Post()
   async create(@Body() createSensorDataDto: CreateSensorDataDto) {
     return this.sensorDataService.createSensorData(createSensorDataDto);
