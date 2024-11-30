2da2f1d07042e5711f26e895a310c2bd6d230f5a
Yutaro Negi <souza_yutaro@hotmail.com>
2024-11-30 14:54:25 -0300

    feat: add SensorDataModule to app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { HealthModule } from './modules/health/health.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
+import { SensorDataModule } from './modules/sensor-data/sensor-data.module';
 
 @Module({
   imports: [
     AuthModule,
+    SensorDataModule,
     HealthModule,
     ConfigModule.forRoot({
       isGlobal: true,
