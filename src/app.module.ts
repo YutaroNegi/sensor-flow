8e8b93d3b2098e101a8f7bdbba270e848b302785
Yutaro Negi <souza_yutaro@hotmail.com>
2024-11-30 09:38:57 -0300

    feat: add auth module to app imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './modules/health/health.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './modules/auth/auth.module';
+
 @Module({
-  imports: [HealthModule, ConfigModule.forRoot()],
+  imports: [AuthModule, HealthModule, ConfigModule.forRoot()],
 })
 export class AppModule {}
