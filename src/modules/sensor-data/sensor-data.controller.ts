613c1f5dafa67bfa8cad565ab6acf4466ee82aa7
Yutaro Negi <souza_yutaro@hotmail.com>
2024-11-30 16:28:33 -0300

    feat: add CSV file upload in sensor-data.controller

--- src/modules/sensor-data/sensor-data.controller.ts
+++ src/modules/sensor-data/sensor-data.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, UseInterceptors, UploadedFile, BadRequestException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 import { SensorDataService } from './sensor-data.service';
 import { CreateSensorDataDto } from './dto/create-sensor-data.dto';
+import { extname } from 'path';
 
 @Controller('sensor-data')
 export class SensorDataController {
@@ -10,5 +13,35 @@ export class SensorDataController {
   @Post()
   async create(@Body() createSensorDataDto: CreateSensorDataDto) {
     return this.sensorDataService.createSensorData(createSensorDataDto);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post('upload-csv')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, callback) => {
+          const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+          const ext = extname(file.originalname);
+          callback(null, `${uniqueSuffix}${ext}`);
+        },
+      }),
+      fileFilter: (req, file, callback) => {
+        if (file.mimetype !== 'text/csv') {
+          return callback(
+            new BadRequestException('Only CSV files are allowed'),
+            false,
+          );
+        }
+        callback(null, true);
+      },
+    }),
+  )
+  async uploadCsv(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('CSV File is required');
+    }
+    return this.sensorDataService.uploadCsvFile(file);
   }
 }
