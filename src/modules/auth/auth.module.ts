25d0bf1e8ebf2f896d29d6fde8b3f28826b49c51
Yutaro Negi <souza_yutaro@hotmail.com>
2024-11-30 11:44:59 -0300

    feat: add AuthController and AuthService to AuthModule

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { JwtStrategy } from './jwt.strategy';
 import { PassportModule } from '@nestjs/passport';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [PassportModule],
-  providers: [JwtStrategy],
+  providers: [AuthService, JwtStrategy],
   exports: [PassportModule],
+  controllers: [AuthController],
 })
 export class AuthModule {}
