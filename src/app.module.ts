647e61c672ac44479361e91d4217e2eae13eb3fa
Yutaro Negi <souza_yutaro@hotmail.com>
2024-11-30 11:46:37 -0300

    refactor: ConfigModule isGlobal=true

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,12 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [AuthModule, HealthModule, ConfigModule.forRoot()],
+  imports: [
+    AuthModule,
+    HealthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
 })
 export class AppModule {}
