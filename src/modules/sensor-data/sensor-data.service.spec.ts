994695bde83f9666fd3c5fd96ac2bd986034203f
Yutaro Negi <souza_yutaro@hotmail.com>
2024-11-30 16:28:53 -0300

    test: add unit test for csv upload in sesor-data.service.spec

--- src/modules/sensor-data/sensor-data.service.spec.ts
+++ src/modules/sensor-data/sensor-data.service.spec.ts
@@ -1,16 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SensorDataService } from './sensor-data.service';
 import { ConfigService } from '@nestjs/config';
-import { DynamoDB } from 'aws-sdk';
+import { DynamoDB, S3 } from 'aws-sdk';
 import { CreateSensorDataDto } from './dto/create-sensor-data.dto';
+
+jest.mock('uuid', () => ({
+  v4: jest.fn(() => 'unique-id'),
+}));
 
 describe('SensorDataService', () => {
   let service: SensorDataService;
   let dynamoDbMock: jest.Mocked<DynamoDB.DocumentClient>;
+  let s3Mock: jest.Mocked<S3>;
 
   beforeEach(async () => {
     dynamoDbMock = {
       put: jest.fn(),
+    } as any;
+
+    s3Mock = {
+      putObject: jest.fn(),
     } as any;
 
     const module: TestingModule = await Test.createTestingModule({
@@ -23,6 +32,9 @@ describe('SensorDataService', () => {
               const config = {
                 AWS_REGION: 'us-east-1',
                 DYNAMODB_TABLE_NAME: 'SensorDataTable',
+                S3_BUCKET_NAME: 'sensor-data-bucket',
+                AWS_ACCESS_KEY_ID: 'test-access-key-id',
+                AWS_SECRET_ACCESS_KEY: 'test-secret-access-key',
               };
               return config[key];
             }),
@@ -31,6 +43,10 @@ describe('SensorDataService', () => {
         {
           provide: 'DynamoDBDocumentClient',
           useValue: dynamoDbMock,
+        },
+        {
+          provide: 'S3Client',
+          useValue: s3Mock,
         },
       ],
     }).compile();
@@ -90,6 +106,62 @@ describe('SensorDataService', () => {
           value: dto.value,
           register_time: expect.any(String),
         },
+      });
+    });
+  });
+
+  describe('uploadCsvFile', () => {
+    it('should upload a csv file successfully', async () => {
+      // Arrange
+      const file: any = {
+        fieldname: 'file',
+        originalname: 'data.csv',
+        encoding: '7bit',
+        mimetype: 'text/csv',
+        buffer: Buffer.from('equipmentId,timestamp,value\nEQ-12495,2023-02-15T01:30:00.000-05:00,78.42'),
+        size: 100,
+      };
+      const expectedResult = { message: 'File uploaded successfully', fileName: 'unique-id-data.csv' };
+      s3Mock.putObject.mockReturnValue({
+        promise: jest.fn().mockResolvedValue({}),
+      } as any);
+
+      // Act
+      const result = await service.uploadCsvFile(file);
+
+      // Assert
+      expect(s3Mock.putObject).toHaveBeenCalledWith({
+        Bucket: 'sensor-data-bucket',
+        Key: 'unique-id-data.csv',
+        Body: file.buffer,
+        ContentType: 'text/csv',
+      });
+      expect(result).toEqual(expectedResult);
+    });
+    it('should return error when S3 fails ', async () => {
+      // Arrange
+      const file: any = {
+        fieldname: 'file',
+        originalname: 'data.csv',
+        encoding: '7bit',
+        mimetype: 'text/csv',
+        buffer: Buffer.from('equipmentId,timestamp,value\nEQ-12495,2023-02-15T01:30:00.000-05:00,78.42'),
+        size: 100,
+      };
+
+      s3Mock.putObject.mockReturnValue({
+        promise: jest.fn().mockRejectedValue(new Error('S3 Error')),
+      } as any);
+
+      // Act & Assert
+      await expect(service.uploadCsvFile(file)).rejects.toThrow(
+        'Error uploading file to S3: S3 Error',
+      );
+      expect(s3Mock.putObject).toHaveBeenCalledWith({
+        Bucket: 'sensor-data-bucket',
+        Key: 'unique-id-data.csv',
+        Body: file.buffer,
+        ContentType: 'text/csv',
       });
     });
   });
