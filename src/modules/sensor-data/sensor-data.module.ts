98244a9b3a151a57c9aa885ef9cfe920128c72c9
Yutaro Negi <souza_yutaro@hotmail.com>
2024-11-30 16:31:46 -0300

    feat: add s3 client provider to sensor-data module

--- src/modules/sensor-data/sensor-data.module.ts
+++ src/modules/sensor-data/sensor-data.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { SensorDataController } from './sensor-data.controller';
 import { SensorDataService } from './sensor-data.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { DynamoDB } from 'aws-sdk';
+import { S3, DynamoDB } from 'aws-sdk';
 import * as https from 'https';
 
 @Module({
@@ -24,6 +24,20 @@ import * as https from 'https';
       },
       inject: [ConfigService],
     },
+    {
+      provide: 'S3Client',
+      useFactory: (configService: ConfigService) => {
+        return new S3({
+          region: configService.get<string>('AWS_REGION'),
+          credentials: {
+            accessKeyId: configService.get<string>('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: configService.get<string>('AWS_SECRET_ACCESS_KEY'),
+          },
+        });
+      },
+      inject: [ConfigService],
+    },
   ],
+
 })
 export class SensorDataModule {}
