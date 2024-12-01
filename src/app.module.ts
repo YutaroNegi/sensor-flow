6de287d17023bd02559b025c418cc571952c3654
Yutaro Negi <souza_yutaro@hotmail.com>
2024-12-01 06:16:15 -0300

    feat: add SensorAggregatedModule to app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { HealthModule } from './modules/health/health.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import { SensorDataModule } from './modules/sensor-data/sensor-data.module';
-
+import { SensorAggregatedModule } from './modules/sensor-aggregated/sensor-aggregated.module';
 @Module({
   imports: [
     AuthModule,
     SensorDataModule,
+    SensorAggregatedModule,
     HealthModule,
     ConfigModule.forRoot({
       isGlobal: true,
