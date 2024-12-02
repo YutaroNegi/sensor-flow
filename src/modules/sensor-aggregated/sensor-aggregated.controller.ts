6c969fcb4f2ca5039899510a974e461dbd73194e
Yutaro Negi <souza_yutaro@hotmail.com>
2024-12-02 11:24:27 -0300

    feat: add auto cache for get route /sensor-data-aggregated

--- src/modules/sensor-aggregated/sensor-aggregated.controller.ts
+++ src/modules/sensor-aggregated/sensor-aggregated.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards, Param, UseInterceptors } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { SensorAggregatedService } from './sensor-aggragated.service';
 import { GetSensorAggregatedDto } from './dto/create-aggregated.dto';
+import { CacheInterceptor } from '@nestjs/cache-manager';
+
 
 @Controller('sensor-data-aggregated')
 export class SensorAggragatedController {
@@ -9,6 +11,7 @@ export class SensorAggragatedController {
     private readonly sensorAggregatedService: SensorAggregatedService,
   ) {}
   @UseGuards(AuthGuard('jwt'))
+  @UseInterceptors(CacheInterceptor)
   @Get()
   async create(@Query() query: GetSensorAggregatedDto) {
     return this.sensorAggregatedService.get(query);
